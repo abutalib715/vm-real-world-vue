67d1d50f111d800662dba562289621a598c59938
Talib <abutalib715@gmail.com>
2022-08-25 23:45:23 +0600

    learned about single file vue component and little bit about composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+  import EventCard from "@/components/EventCard.vue";
+  
+  const events = [
+    {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline',
+          location: 'Meow Toen',
+          date: 'January 28 2023',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+      },{
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline',
+          location: 'Meow Toen',
+          date: 'January 28 2023',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+      },{
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline',
+          location: 'Meow Toen',
+          date: 'January 28 2023',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+      }
+  ]
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </main>
 </template>
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
