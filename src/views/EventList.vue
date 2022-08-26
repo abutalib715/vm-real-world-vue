6eb3b0fd4743312db6d7bc4a1db980ed792bc5bf
Talib <abutalib715@gmail.com>
2022-08-26 23:19:06 +0600

    fetched event data using api call

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,39 +1,14 @@
 <script setup>
-  import EventCard from "@/components/EventCard.vue";
-  
-  const events = [
-    {
-          id: 5928101,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline',
-          location: 'Meow Toen',
-          date: 'January 28 2023',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kat Laydee'
-      },{
-          id: 5928101,
-          category: 'Laravel',
-          title: 'Laracon Asia',
-          description: 'lorem text for test',
-          location: 'Mumbai',
-          date: 'December 28 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Unacademy'
-      },{
-          id: 5928101,
-          category: 'Wordpress',
-          title: 'WordCamp-2022',
-          description: 'Find your new feline',
-          location: 'Dhaka',
-          date: 'December 20 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'W3Devs'
-      }
-  ]
+  import { ref } from 'vue'
+  import EventCard from "@/components/EventCard.vue"
+  import EventService from '@/services/EventService.js'
+
+  const events = ref(null);
+  EventService.getEvents().then(response => {
+      events.value = response.data
+  }).catch(error => {
+      console.log(error);
+  });
 </script>
 
 <template>
