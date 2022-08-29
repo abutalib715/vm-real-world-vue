866bfed50e6cf8c0ce1f77ee2035f9dd85194738
Talib <abutalib715@gmail.com>
2022-08-29 09:17:17 +0600

    learned auto importing components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 import router from "./router";
 
 import "./assets/main.css";
 
 const app = createApp(App);
+
+// // Getting all form component that starts with Base
+// const requireComponent = require.context(
+//     './components/forms/',
+//     false,
+//     /Base[A-Z]\w+\.(vue|js)$/)
+
+// //importing all component related to form 
+// requireComponent.keys().forEach(fileName => {
+//     const componentConfig = requireComponent(fileName)
+//     const componentName = upperFirst(
+//         camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+//     )
+//     app.component(componentName,componentConfig.default || componentConfig)
+// });
 
 app.use(router);
 
